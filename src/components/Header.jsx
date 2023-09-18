5f74ffa0504f18889d991ca7e3f221f843955d06
Lowell Alexander <performance@bidhackers.com>
2023-09-18 14:56:45 -0400

    added footer and fixed some of the small styling issues

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import reactLogo from "../assets/react.svg";
 export default function Header() {
   return (
     <>
-      <div className="container" id="header">
+      <div className="" id="header">
         <img src={reactLogo} className="logo react" alt="React logo" />
         <div id="header-right">
           <p>About</p>
